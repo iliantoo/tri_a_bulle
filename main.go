36bd0e496336279569539f76ed3004613142afb1
iliantoo <iliantouazi@gmail.com>
2025-09-24 11:48:09 +0200

    test

--- main.go
+++ main.go
@@ -1,19 +1,8 @@
 package main
 
-import(
-	"fmt"
-)
-
 func main () {
-	x := []int{20, 30, 10, 40}
-	for i := 0; i < len(x); i++ {
-		n := x[i]
-		fmt.Printf("valeur tableau : %d\n", n)
-	}
+
+    // toto
+
 }
 
-
-/*
-[64,34,25,12,22,11,90]
-[0 , 1]
-*/

bec9ccade7861de13c47ba1994d7ec4e2db4e101
cjoaquin <joaquin.cozette@ynov.com>
2025-09-24 11:49:10 +0200

    test2

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
+import(
+	"fmt"
+)
+
 func main () {
-
-    // toto
-
+	x := []int{20, 30, 10, 40}
+	for i := 0; i < len(x); i++ {
+		n := x[i]
+		fmt.Printf("valeur tableau : %d\n", n)
+	}
 }
 
+
+/*
+[64,34,25,12,22,11,90]
+[0 , 1]
+*/
